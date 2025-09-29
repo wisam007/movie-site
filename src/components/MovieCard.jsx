810ae833c6b9264fe928bd45a7a78410008404e9
Wissam <wissamjemal99@gmail.com>
2025-09-29 14:38:34 +0300

    Commit

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -24,8 +24,11 @@ const MovieCard = ({
         <h3>{title}</h3>
         <div className="content">
           <div className="rating">
-            <img src="star.svg" />
+            <img src="star.svg" alt="Star Icon" />
+            <p>{vote_average ? vote_average.toFixed(1) : "N/A"}</p>
           </div>
+          <span>•</span> <span>•</span>
+          <p className="lang">{original_language}</p>
         </div>
       </div>
     </div>
